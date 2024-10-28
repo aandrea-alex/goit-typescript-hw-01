200b92d57647bc8eb11ed03a883d82f39f6f83a6
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-10-28 19:53:53 +0100

    task 5

---
+++ src/basic/5.ts
@@ -0,0 +1,16 @@
+enum DayOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  function isWeekend(day: DayOfWeek): boolean {
+    return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+  }
+  
+  export { DayOfWeek, isWeekend };
+  
